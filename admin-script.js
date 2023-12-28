e19821cd478df9c286cdf8a66288edda6d7c9e6b
Rashmi-kumari5 <101795659+Rashmi-kumari5@users.noreply.github.com>
2023-12-28 17:19:56 +0530

    removed admin

--- admin-script.js
+++ admin-script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const addUserForm = document.getElementById('addUserForm');
   const existingUsersDiv = document.getElementById('existingUsers');
-  const usersSet = new Set(); // Use Set to store unique usernames
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+
+  // Display existing users on page load
+  displayExistingUsers();
 
   // Function to add a new user
   addUserForm.addEventListener('submit', (event) => {
@@ -9,77 +12,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
 
-    if (username !== '' && !usersSet.has(username)) {
-      // Add the user if the username is not empty and is unique
-      const newUser = { username, password };
-      addUser(newUser);
+    if (username !== '' && password !== '') {
+      if (!isDuplicateUsername(username)) {
+        const newUser = { username, password };
+        users.push(newUser);
+        localStorage.setItem('users', JSON.stringify(users));
+        displayExistingUsers();
+        addUserForm.reset();
+      } else {
+        alert('Username already exists. Please choose a different username.');
+      }
     } else {
-      alert('Please enter a unique username.');
+      alert('Please enter both username and password.');
     }
   });
 
-  // Function to add a user to the list
-  function addUser(user) {
-    usersSet.add(user.username); // Add username to the Set
-    const userDiv = document.createElement('div');
-    userDiv.classList.add('user');
-    userDiv.innerHTML = `<strong>Username:</strong> ${user.username}`;
-    existingUsersDiv.appendChild(userDiv);
+  // Function to check for duplicate usernames
+  function isDuplicateUsername(username) {
+    return users.some(user => user.username === username);
   }
 
-  // Function to display existing users (if you have stored data in an array, replace this part)
+  // Function to display existing users
   function displayExistingUsers() {
-    // Simulated data for example purposes
-    const existingUsers = [
-      { username: 'admin', password: 'admin123' },
-      // Add more users here
-    ];
-
-    existingUsers.forEach(user => {
-      if (!usersSet.has(user.username)) {
-        addUser(user);
-      }
+    existingUsersDiv.innerHTML = '';
+    const uniqueUsernames = [...new Set(users.map(user => user.username))];
+    uniqueUsernames.forEach(username => {
+      const userDiv = document.createElement('div');
+      userDiv.classList.add('user');
+      userDiv.innerHTML = `<strong>Username:</strong> ${username}`;
+      
+      // Add delete button for each user
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.classList.add('delete-btn');
+      deleteButton.addEventListener('click', () => {
+        deleteUser(username);
+      });
+      
+      userDiv.appendChild(deleteButton);
+      existingUsersDiv.appendChild(userDiv);
     });
   }
 
-  // Call the function to display existing users on page load
-  displayExistingUsers();
-});
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const modelSelect = document.getElementById('modelNo');
-  const addModelBtn = document.getElementById('addModelBtn');
-  const deleteModelBtn = document.getElementById('deleteModelBtn');
-
-  // Function to add a new model
-  addModelBtn.addEventListener('click', () => {
-    const newModelNumber = prompt('Enter the Model Number:');
-    if (newModelNumber !== null && newModelNumber !== '') {
-      const option = document.createElement('option');
-      option.value = newModelNumber;
-      option.textContent = `Model ${newModelNumber}`;
-      modelSelect.appendChild(option);
-    }
-  });
-
-  // Function to delete the selected model
-  deleteModelBtn.addEventListener('click', () => {
-    const selectedModel = modelSelect.value;
-    if (selectedModel !== '') {
-      const confirmation = confirm(`Are you sure you want to delete Model ${selectedModel}?`);
-      if (confirmation) {
-        const options = modelSelect.options;
-        for (let i = 0; i < options.length; i++) {
-          if (options[i].value === selectedModel) {
-            modelSelect.removeChild(options[i]);
-            break;
-          }
-        }
-      }
-    } else {
-      alert('Please select a model to delete.');
-    }
-  });
+  // Function to delete a user
+  function deleteUser(username) {
+    users = users.filter(user => user.username !== username);
+    localStorage.setItem('users', JSON.stringify(users));
+    displayExistingUsers();
+  }
 });
